5f01e914281b9ce506ba9316ec1d62affb88b48d
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-11 18:30:56 -0500

    refactored entire project to have more structure and to be better documented

---
+++ README.md
@@ -0,0 +1,3 @@
+### Empty Trash
+
+Python script to empty local trash.  Might try to automate via Cron.
--- empty.py
+++ empty.py
@@ -1,22 +1,38 @@
+############------------ IMPORTS ------------############
 import os
 import shutil
-# shutil documentatio: https://docs.python.org/3/library/shutil.html
 
+
+############------------ GLOBAL VARIABLE(S) ------------############
 # terminal must have full disk access 
 path_to_trash = "/Users/aaronaguerrevere/.Trash"
 
 files_in_trash = os.listdir(path_to_trash)
 
-for file in files_in_trash:
-    files_in_trash_path = os.path.join(path_to_trash, file)
-    try:
-        if os.path.isfile(files_in_trash_path):
-            os.remove(files_in_trash_path)
-            print("File successfully deleted")
-        elif os.path.isdir(files_in_trash_path):
-            shutil.rmtree(files_in_trash_path)
-            print("Directory successfully deleted")
-    except Exception as e:
-        print(e)
 
-print("Trash emptied!")
+############------------ FUNCTION(S) ------------############
+def buh_bye_trash():
+    for file in files_in_trash:
+        # all files in trash
+        files_in_trash_path = os.path.join(path_to_trash, file)
+
+        # if is file, remove it and let the user know
+        try:
+            if os.path.isfile(files_in_trash_path):
+                os.remove(files_in_trash_path)
+                print("File successfully deleted")
+            # otherwise, remove whole directory
+            elif os.path.isdir(files_in_trash_path):
+                shutil.rmtree(files_in_trash_path)
+                print("Directory successfully deleted")
+
+        except Exception as e:
+            print(e)
+
+    # once done, let the user know
+    print("Trash emptied!")
+
+
+############------------ DRIVER CODE ------------############
+if __name__ == "__main__":
+    buh_bye_trash()

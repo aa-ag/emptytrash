1f7425419b7abe8207edf37156af36b4ba6b2a31
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-11 18:40:35 -0500

    empty.py working as expected;  needed to provide visual studio code full disk access

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### Empty Trash
 
 Python script to empty local trash.  Might try to automate via Cron.
+
+### to run
+
+Full Disk Access is needed for this program to run.
--- empty.py
+++ empty.py
@@ -1,27 +1,28 @@
 ############------------ IMPORTS ------------############
 import os
 import shutil
+import settings
 
 
 ############------------ GLOBAL VARIABLE(S) ------------############
 # terminal must have full disk access 
-path_to_trash = "/Users/aaronaguerrevere/.Trash"
+trash = settings.path_to_trash
 
-files_in_trash = os.listdir(path_to_trash)
+files_in_trash = os.listdir(trash)
 
 
 ############------------ FUNCTION(S) ------------############
 def buh_bye_trash():
     for file in files_in_trash:
         # all files in trash
-        files_in_trash_path = os.path.join(path_to_trash, file)
+        files_in_trash_path = os.path.join(trash, file)
 
         # if is file, remove it and let the user know
         try:
             if os.path.isfile(files_in_trash_path):
                 os.remove(files_in_trash_path)
                 print("File successfully deleted")
-            # otherwise, remove whole directory
+            # otherwise, remove whole empty directory
             elif os.path.isdir(files_in_trash_path):
                 shutil.rmtree(files_in_trash_path)
                 print("Directory successfully deleted")
@@ -33,6 +34,6 @@ def buh_bye_trash():
     print("Trash emptied!")
 
 
-############------------ DRIVER CODE ------------############
+# ############------------ DRIVER CODE ------------############
 if __name__ == "__main__":
     buh_bye_trash()

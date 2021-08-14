f43ba370adbcce8d33c6939ad6eb5d91c13a810a
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-13 20:06:45 -0500

    added 10 seconds sleep after each task to be able to see/look at progress a little slower

--- app.py
+++ app.py
@@ -17,12 +17,15 @@ def queue_tasks():
     
     while loops < 3:
         queue.enqueue(tasks.create_test_files, retry=Retry(max=2))
+        time.sleep(10)
 
         queue.enqueue_in(timedelta(seconds=5), tasks.delete_test_files)
 
+        time.sleep(10)
         queue.enqueue_in(timedelta(seconds=5), tasks.buh_bye_trash)
 
         loops += 1
+        time.sleep(10)
     
 
 

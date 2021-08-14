c78b64aadae6ed3b9faed615ad33fad5262be858
aa-ag <aaron.aguerrevere@gmail.com>
2021-08-13 19:57:32 -0500

    added logic to enqueue tasks in app.py

--- app.py
+++ app.py
@@ -13,7 +13,14 @@ queue = Queue(connection=Redis())
 
 ############------------ FUNCTION(S) ------------############
 def queue_tasks():
-    pass
+    loops = 0
+    
+    while loops < 3:
+        queue.enqueue(tasks.create_test_files, 5, retry=Retry(max=2))
+        queue.enqueue_in(timedelta(seconds=5), tasks.delete_test_files, 5)
+        queue.enqueue_in(timedelta(seconds=5), tasks.buh_bye_trash, 5)
+
+        loops += 1
     
 
 
